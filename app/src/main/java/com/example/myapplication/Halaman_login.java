4241dc2e3f19706d5c6834d29c3347f62dd411ae
Kahfi31 <kahfi032004.kb.kb@gmail.com>
2024-04-20 22:07:33 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/Halaman_login.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Halaman_login extends AppCompatActivity {
+
+    private EditText btnpw;
+
+    @SuppressLint("CutPasteId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_halaman_login);
+
+        btnpw = findViewById(R.id.btnpw);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login(View view) {
+        Intent login = new Intent(Halaman_login.this, Halaman_Utama.class);
+        startActivity(login);
+    }
+
+    public void kembali(View view) {
+        Intent kembali = new Intent(Halaman_login.this, Halaman_pilihan.class);
+        startActivity(kembali);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/Halaman_register.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Halaman_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_halaman_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void register(View view) {
+        Intent register = new
+                Intent(Halaman_register.this, Halaman_pilihan.class);
+        startActivity(register);
+    }
+
+    public void kembali(View view) {
+        Intent kembali = new
+                Intent(Halaman_register.this, Halaman_pilihan.class);
+        startActivity(kembali);
+    }
+
+}
